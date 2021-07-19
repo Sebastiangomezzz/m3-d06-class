0035b01e40780fd78c5101085c63f519d4f5ab78
sebastiangomezzz <sebastiangomezzz@gmail.com>
2021-07-19 15:17:40 +0200

    "mas cosas"

--- server/config/index.js
+++ server/config/index.js
@@ -50,7 +50,7 @@ module.exports = (app) => {
 				maxAge: 30 * 24 * 60 * 60 * 1000
 			},
 			store: MongoStore.create({
-				mongoUrl: process.env.MONGODB_URI || 'mongodb://localhost/auth-demo'
+				mongoUrl: process.env.MONGODB_URI || 'mongodb://localhost/iron-project'
 			})
 		})
 	);
--- server/routes/project.routes.js
+++ server/routes/project.routes.js
@@ -1,21 +1,21 @@
-const { json } = require('express');
+// routes/project.routes.js
+ 
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 
-const Project = require('../models/Project.model');
+const Project = require('../models/Project.model')
 
 router.post('/', (req, res)=>{
     const { title, description } = req.body
-    
+
     Project.create({
         title,
         description
     })
-    .then(
-        createdProject => res.json(createdProject)
-    )
-    .catch(err=>json(err))
+    .then( createdProject => res.json(createdProject))
+    .catch(err=>res.json(err))
 })
 
 module.exports = router
+// the same as 'export defualt' that we have used in frontend code
